4822b883929ee55041d8723149b116e05c254a74
juacc <juacc@juacc.net>
2026-04-05 21:17:24 -0300

    Fixed colorizer

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,34 +1,40 @@
 -- Colorize color codes
 return {
-    -- "norcalli/nvim-colorizer.lua",
+    "catgoose/nvim-colorizer.lua",
 
-    -- event = { "BufReadPre", "BufNewFile" },
+    event = "BufReadPre",
 
-    -- keys = {
-    --     { "<leader>lc", vim.cmd.ColorizerReloadAllBuffers },
-    --     desc = "Reload colorizer"
-    -- },
+    keys = {
+        { "<leader>lc", vim.cmd.ColorizerReloadAllBuffers },
+        desc = "Reload colorizer"
+    },
 
-    -- config = function()
-    --     vim.opt.termguicolors = true
+    config = function()
+        vim.opt.termguicolors = true
 
-    --     require("colorizer").setup(
-    --     -- Filetypes for which the plugin is enabled.
-    --     -- They can have overrides for the global options
-    --         { "*" },
-
-    --         -- Options, they are documented in `:h colorizer-options`
-    --         {
-    --             RGB      = true,
-    --             RRGGBB   = true,
-    --             names    = true,
-    --             RRGGBBAA = true,
-    --             rgb_fn   = false,
-    --             hsl_fn   = false,
-    --             css      = false,
-    --             css_fn   = false,
-    --             mode     = "background",
-    --         }
-    --     )
-    -- end,
+        require("colorizer").setup({
+            options = {
+                parsers = {
+                    names = {
+                        enable = false, -- enable named colors (e.g. "Blue")
+                    },
+                    hex = {
+                        hash_aarrggbb = true, -- #AARRGGBB (QML-style, alpha first)
+                        aarrggbb = true, -- 0xAARRGGBB
+                    },
+                    rgb = { enable = true }, -- rgb()/rgba() functions
+                    hsl = { enable = true }, -- hsl()/hsla() functions
+                    xterm = { enable = true }, -- xterm 256-color codes (#xNN, \e[38;5;NNNm)
+                    css_var_rgb = { enable = true }, -- CSS vars with R,G,B (e.g. --color: 240,198,198)
+                },
+                display = {
+                    mode = "background", -- string or list: "background"|"foreground"|"underline"|"virtualtext"
+                    background = {
+                        bright_fg = "#1d2021", -- text color on bright backgrounds
+                        dark_fg = "#ebdbb2", -- text color on dark backgrounds
+                    },
+                },
+            },
+        })
+    end,
 }
